66d9c6d385bf2ae5d0be78c1aa44d520c6bd0692
zohaiblogin <zohaib.login@gmail.com>
2018-02-15 17:48:39 +0500

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { routes } from './app.router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/shared/navbar/navbar.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,9 +2,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Component } from '@angular/core/src/metadata/directives';
 import { HomeComponent } from './component/home/home.component';
+import { ContactComponent } from './component/contact/contact.component';
+
 
 export const router: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'contact-us', component: ContactComponent },
+  
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
